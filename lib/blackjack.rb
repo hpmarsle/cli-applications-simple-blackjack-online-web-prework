f281f6001e3d485c4d4beacf580793de6d4f22bf
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-08-07 02:23:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,10 +32,10 @@ end
 
 def hit? (card_total)
   prompt_user
-  get_user_input
-  if get_user_input == 'h'.downcase
+  choice = get_user_input
+  if choice == 'h'.downcase
     card_total += deal_card
-  elsif get_user_input =='s'.downcase
+  elsif choice =='s'.downcase
     card_total
   else
     invalid_command
